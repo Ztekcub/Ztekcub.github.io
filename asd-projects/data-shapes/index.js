b7c3701e7624735e99f307b41e50fdee7100d710
Ztekcub <Mauricekwj@gmail.com>
2024-03-07 21:52:38 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -36,15 +36,31 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
-
+  // TODO 0 complete
   // TODO 1: create a new shape object and add it to the array
-  
-
+  var shape = {
+    color: "blue",
+    shape: "circle",
+    repeat: 3,
+  }
+dataShapes.push(shape)
   // TODO 2: add a new property to all data shapes
   
+  for (var i = 0; i < dataShapes.length; i++ {
+    var currentShape = dataShapes[i];
+
+    if (currentShape[color] === "red"){
+       currentShape.goodBehavior = "bounce"
+    }else if (currentShape[color] === "blue"){
+      currentShape.goodBehavior = "blink"
+  }else {
+    currentShape.goodBehavior = "spin"
+  }
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStactic (data){
+    
+  }
 
   // TODO 4-a: add a function that handles the good display type
   
